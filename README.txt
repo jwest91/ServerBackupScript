166ca39f1d4403e211553af996ab95fe2aa728ee
Justin West <jwest91@hotmail.com>
2020-02-15 15:30:46 -0600

    Initial commit - testing basic functionality

--- README.txt
+++ README.txt
@@ -1,2 +1,6 @@
 # ServerBackupScript
  Small Python script to periodicaly backup my Minecraft server files to a different drive.
+
+
+Change Log:
+* initial script: 02/15/2020
---
+++ backupScript.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+#Where to get the fiels from to back up (sample path)
+sourcePath = "/users/justinwest/Desktop/Server/"
+
+#Where to copy the files to (sample path)
+destinationPath = "/Volumes/SERVERFILES/"
+
+#Get the files in the source directory 
+sourceFiles = os.listdir(sourcePath)
+
+#Loop through the files
+for individualFile in sourceFiles:
+	fullFileName = os.path.join(sourcePath, individualFile)
+
+	#If they are legitimate files, copy them to destination directory
+	if os.path.isfile(fullFileName):
+		shutil.copy(fullFileName, destinationPath)
+
